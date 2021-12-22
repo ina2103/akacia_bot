8713083f6f9e6b0abacc847d1d2d0b81af9b0120
ina2103 <ivanu6ka@gmail.com>
2021-12-22 22:08:09 +0500

    [FIX] cashbox checking

--- akacia_aot.py
+++ akacia_aot.py
@@ -148,8 +148,10 @@ def command_transfer(update: Update, context: CallbackContext):
         telegram_send(context.bot, chat_id, TEMPLATE_MANAGER_NO_PERMISSION)
         return ConversationHandler.END
     cashboxes = select_allowed_cashboxes(staff_id)
-    cashboxes_from = [cashbox["cashbox_name"] for cashbox in cashboxes if cashbox["cashbox_is_cash"] and cashbox["can_transfer_from"]]
-    cashboxes_to = [cashbox["cashbox_name"] for cashbox in cashboxes if cashbox["cashbox_is_cash"] and cashbox["can_transfer_to"]]
+    cashboxes_from_temp = [cashbox["cashbox_name"] for cashbox in cashboxes if cashbox["cashbox_is_cash"] and cashbox["can_transfer_from"]]
+    cashboxes_to_temp = [cashbox["cashbox_name"] for cashbox in cashboxes if cashbox["cashbox_is_cash"] and cashbox["can_transfer_to"]]
+    cashboxes_from = [cashbox for cashbox in cashboxes_from_temp if cashbox not in cashboxes_to_temp]
+    cashboxes_to = [cashbox for cashbox in cashboxes_to_temp if cashbox not in cashboxes_from_temp]
     if (len(context.args) == 1):
         from_cashbox = next((cashbox for cashbox in cashboxes_from if cashbox["cashbox_name"]=="anna"), None)
         to_cashbox = next((cashbox for cashbox in cashboxes_to if cashbox["cashbox_name"]=="mike"), None)
@@ -158,12 +160,11 @@ def command_transfer(update: Update, context: CallbackContext):
             context.user_data["to_cashbox"] = to_cashbox
             update.message.text = context.args[0]
             return conversation__transfer_sum(update, context)
-    if len(cashboxes_from) == 1 and len(cashboxes_to) == 1 \
-        and cashboxes_from[0]["cashbox_name"] != cashboxes_to[0]["cashbox_name"]:
+    if len(cashboxes_from) == 1 and len(cashboxes_to) == 1:
         update.message.text = f'{cashboxes_from[0]["cashbox_name"]} > {cashboxes_to[0]["cashbox_name"]}'
         return conversation__transfer_cashboxes(update, context)
     if len(cashboxes_from) >= 1 and len(cashboxes_to) >= 1:
-        buttons = [[f"{x} > {y}"] for x in cashboxes_from for y in cashboxes_to if x != y]
+        buttons = [[f"{x} > {y}"] for x in cashboxes_from for y in cashboxes_to]
         reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
         telegram_send(context.bot, chat_id, TEMPLATE_SELECT_CASHBOXES, reply_markup=reply_markup)
         return WAITING_CASHBOX
